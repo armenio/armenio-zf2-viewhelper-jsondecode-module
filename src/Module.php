2931548870e06029d410ac0e71edd1aa265f6a6c
Rafael Armenio <rafael.armenio@gmail.com>
2016-05-21 16:08:26 -0300

    move Module.php (psr-4 namespace)

--- src/Module.php
+++ src/Module.php
@@ -11,6 +11,6 @@ class Module
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include dirname(__DIR__) . '/config/module.config.php';
     }
 }
